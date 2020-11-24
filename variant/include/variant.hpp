e3f96120bf121481038304e0389ff33a2b09846b
angemessener-spitzname <62075628+angemessener-spitzname@users.noreply.github.com>
2020-11-24 16:23:42 +0300

    Update variant.hpp

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -1,7 +1,7 @@
 /* Variant classes.
- * @file
- * @date 2018-08-12
- * @author Anonymous
+ * @file variant
+ * @date 2020
+ * @author Серафима Рысина
  */
 
 #ifndef __VARIANT_HPP__
@@ -10,55 +10,41 @@
 #include <map>
 #include <memory>
 #include <variant>
-#include <vector>
+#include <type_traits>
+#include <cstdint>
+#include <deque>
 
 #include <boost/variant.hpp>
 
-//{ number
-struct number
+struct number : public std::variant<int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t, float, double, long double>
+{ using variant::variant; };
+
+struct array : public std::deque<number>
+{ using deque::deque; };
+
+struct recursive_array : public std::deque<std::variant<number, std::shared_ptr<recursive_array>>>
+{ using deque::deque; };
+
+struct recursive_array2 : public std::deque<std::variant<number, boost::recursive_wrapper<recursive_array2>>>
+{ using deque::deque; };
+
+template<typename... Args> struct variant_decorator : public std::variant<Args...>
 {
+    using variant_decorator::variant::variant;
+    
+    template<typename T> T& as()
+    { return const_cast<T&>(const_cast<const variant_decorator*>(this)->as<T>()); }
 
-};
-//}
-
-//{ array
-struct array
-{
-
-};
-//}
-
-//{ recursive_array
-struct recursive_array
-{
-
-};
-//}
-
-//{ recursive_array2
-struct recursive_array2
-{
-
-};
-//}
-
-//{ variant_decorator
-template<???>
-struct variant_decorator
-{
-
-    ?? as()
+    template<typename T> T const& as() const
     {
-
+        if constexpr (std::disjunction_v<std::is_same<boost::recursive_wrapper<T>, Args>...>)
+            return std::get<boost::recursive_wrapper<T>>(*this).get();
+        else
+            return std::get<T>(*this);
     }
 };
-//}
 
-//{ recursive_map
-struct recursive_map
-{
-
-};
-//}
+struct recursive_map : public std::map<std::string, variant_decorator<bool, int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t, uint64_t, float, double, long double, std::string, boost::recursive_wrapper<recursive_map>>>
+{ using map::map; };
 
 #endif // __VARIANT_HPP__
