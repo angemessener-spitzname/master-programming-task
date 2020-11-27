c2cec69dd5bfca79cb639926778df29d46795189
angemessener-spitzname <62075628+angemessener-spitzname@users.noreply.github.com>
2020-11-27 23:23:21 +0300

    Update iterator.hpp

--- iterator/include/iterator.hpp
+++ iterator/include/iterator.hpp
@@ -1,7 +1,7 @@
 /* Image iterator.
  * @file
- * @date 2018-08-05
- * @author Anonymous
+ * @date 2020
+ * @author Серафима Рысина
  */
 
 #ifndef __ITERATOR_HPP__
@@ -14,13 +14,53 @@
 
 #include <boost/iterator/iterator_adaptor.hpp>
 
-//{ image iterator
-template<class Iterator>
-class image_iterator: public boost::iterator_adaptor<...>
-{
+#include <iostream>
 
+template<typename T>
+class image_iterator : public boost::iterator_adaptor<image_iterator<T>, T>
+{
+    public:
+        image_iterator(T it, size_t w, size_t st)
+            : boost::iterator_adaptor<image_iterator<T>, T>(it)
+            , start_iterator_(it)
+            , w_(w)
+            , st_(st)
+        {}
+
+        long distance_to(const image_iterator& it) const
+        {
+            const auto d1 = std::abs(it.base() - it.start_iterator_) % ptrdiff_t(st_);
+            const auto d2 = std::abs(this->base() - start_iterator_) % ptrdiff_t(st_);
+
+            return (it.base() - d1 - (this->base() - d2)) / ptrdiff_t(st_) * w_  + (d1 - d2);
+        }
+
+        void advance(const int n)
+        {
+            if (n > 0) for (int i = 0; i < n; ++i) increment();            
+            else for (int i = 0; i < -n; ++i) decrement();
+        }
+
+        void decrement()
+        {
+            if (std::abs(this->base() - start_iterator_) % ptrdiff_t(st_) <= 0)
+                this->base_reference() -= (st_ - w_ + 1);
+            else
+                --this->base_reference();
+        }
+
+        void increment() {
+            if (std::abs(this->base() - start_iterator_) % ptrdiff_t(st_) + 1 >= long(w_))
+                this->base_reference() += (st_ - w_ + 1);
+            else
+                ++this->base_reference();
+        }                
+
+    private:
+        T start_iterator_;
+        size_t w_;
+        size_t st_;
 };
-//}
 
 template<class Container = std::vector<uint8_t>>
 class image
