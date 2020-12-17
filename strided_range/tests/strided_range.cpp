b61fba485d83a9b3f7d01b2cb55b2c52fe12fb48
angemessener-spitzname <62075628+angemessener-spitzname@users.noreply.github.com>
2020-12-17 14:48:43 +0300

    Update strided_range.cpp

--- strided_range/tests/strided_range.cpp
+++ strided_range/tests/strided_range.cpp
@@ -11,51 +11,38 @@
 
 // Two checks with different strides.
 TEST_CASE("strided_range::stride") {
-    const size_t N = 1000;
-    std::vector<int> rng;
-    rng.assign(N, 0);
+    std::vector rng{7, 8, -9, 12, 35, 96, 1125, -147, 1};
 
-    const size_t width = 5;
+    const size_t width = 2;
 
     SECTION("stride > width") {
-        const size_t stride = 6;
+        const size_t stride = 3;
         const auto range = make_strided_range(rng, width, stride);
-        for (const auto i : range) {
-            CHECK(i == 0);
-        }
+        CHECK( range == std::vector{7, 8, 12, 35, 1125, -147} );
     }
 
     SECTION("stride < width") {
         const size_t stride = 1;
         const auto range = make_strided_range(rng, width, stride);
-        for (const auto i : range) {
-            CHECK(i == 0);
-        }
+        CHECK( range == std::vector{7, 8, -9, 12, 35, 96, 1125, -147, 1} );
     }
 }
 
 // Two checks with different widths.
 TEST_CASE("strided_range::width") {
-    const size_t N = 1000;
-    std::vector<int> rng;
-    rng.assign(N, 0);
-
-    const size_t stride = 5;
+    std::vector<int> rng{7, 8, -9, 12, 35, 96, 1125, -147, 1};
+    const size_t stride = 2;
 
     SECTION("stride > width") {
-        const size_t width = 6;
+        const size_t width = 1;
         const auto range = make_strided_range(rng, width, stride);
-        for (const auto i : range) {
-            CHECK(i == 0);
-        }
+        CHECK( range == std::vector{7, -9, 35, 1125, 1} );
     }
 
     SECTION("stride < width") {
-        const size_t width = 1;
+        const size_t width = 3;
         const auto range = make_strided_range(rng, width, stride);
-        for (const auto i : range) {
-            CHECK(i == 0);
-        }
+        CHECK( range == std::vector{7, 8, -9, 12, 35, 96, 1125, -147, 1} );
     }
 }
 
@@ -69,9 +56,7 @@ TEST_CASE("strided_range::stride_equal_width") {
 
     const auto range = make_strided_range(rng, width, stride);
 
-    for (const auto el : range) {
-        CHECK(el == 0);
-    }
+    CHECK(rng == range);
 }
 
 // Use boost::size.
@@ -85,47 +70,39 @@ TEST_CASE("strided_range::distance") {
 
     const auto range = make_strided_range(rng, width, stride);
 
-    CHECK(boost::size(range) != boost::size(rng) );
+    CHECK( boost::size(range) == 834 );
 }
 
 // Make strided_range from another strided_range.
 TEST_CASE("strided_range::from_strided") {
-    const size_t N = 1000;
-    std::vector<int> rng;
-    rng.assign(N, 0);
+    std::vector<int> rng{7, 8, -9, 12, 35, 96, 1125, -147, 1};
+    const size_t stride = 2;
+    const size_t width = 1;
 
-    const size_t width = 5;
-    const size_t stride = 6;
 
     const auto range1 = make_strided_range(rng, width, stride);
     const auto range2 = make_strided_range(range1, width, stride);
-    
-    for (const auto el : range2) {
-        CHECK(el == 0);
-    }
+
+    CHECK(range2 == std::vector{7, 35, 1});    
 }
 
 // Provide example with shifted data. It's like a window in the image. Hint: boost::make_iterator_range.
 TEST_CASE("strided_range::shift") {
-    const size_t N = 1000;
-    std::vector<int> rng;
-    rng.assign(N, 0);
-
-    const size_t width = 5;
-    const size_t stride = 6;
+    std::vector<int> rng{7, 8, -9, 12, 35, 96, 1125, -147, 1};
+    const size_t stride = 2;
+    const size_t width = 1;
 
     const auto range = make_strided_range(rng, width, stride);
     
     auto begin = range.begin();
     auto end = range.begin();
-    boost::advance(begin, 100);
-    boost::advance(end, 200);
+    boost::advance(begin, 1);
+    boost::advance(end, 3);
 
     const auto new_range = boost::make_iterator_range(begin, end);
 
-    for (const auto el : new_range) {
-        CHECK(el == 0);
-    }
+    CHECK(new_range == std::vector{-9, 35});
+
 }
 
 // if call make_strided_range with zero on 3 parametr, will be devision by zero and undefined behavior
